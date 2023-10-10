60593f80c00d87193a99023fd65c9144670deeaf
mugidar <mugidar@gmail.com>
2023-10-10 21:02:46 +0300

    Header. Intro

---
+++ src/components/Header.tsx
@@ -0,0 +1,38 @@
+'use client'
+import { motion } from 'framer-motion'
+import React from 'react'
+import { links } from '../../lib/data'
+import Link from 'next/link'
+
+const Header = () => {
+	return (
+		<header className="z-[999] relative">
+			<motion.div
+				initial={{ y: -100, x: '-50%', opacity: 0 }}
+				animate={{ y: 0, opacity: 1 }}
+				className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.003] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full"
+			></motion.div>
+			<nav className="flex fixed top-[0.15rem] left-1/2 h-12  -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
+				<ul className="flex h-full justify-center flex-wrap w-[22rem] gap-x-5 gap-y-1 text-[0.9rem] font-medium sm:flex-nowrap ">
+					{links.map(link => (
+						<motion.li
+							initial={{ y: -100, opacity: 0 }}
+							animate={{ y: 0, opacity: 1 }}
+							className="h-3/4 flex items-center justify-center "
+						>
+							<Link
+								className="flex justify-center w-full items-center px-3 py-3 transition hover:text-gray-400"
+								key={link.hash}
+								href={link.hash}
+							>
+								{link.name}
+							</Link>
+						</motion.li>
+					))}
+				</ul>
+			</nav>
+		</header>
+	)
+}
+
+export default Header
---
+++ src/components/Intro.tsx
@@ -0,0 +1,110 @@
+'use client'
+import Image from 'next/image'
+import React from 'react'
+import { motion } from 'framer-motion'
+import Link from 'next/link'
+import { BsArrowRight, BsGithub, BsLinkedin } from 'react-icons/bs'
+import { HiDownload } from 'react-icons/hi'
+const Intro = () => {
+	return (
+		<section className="max-w-[50rem] text-center sm:mb-0">
+			<div className="flex flex-col items-center justify-center">
+				<div className="relative">
+					<motion.div
+						className="relative h-36 w-36"
+						transition={{
+							type: 'tween',
+							duration: 0.2
+						}}
+						initial={{ scale: 0, opacity: 0 }}
+						animate={{ scale: 1, opacity: 1 }}
+					>
+						<Image
+							className="translate object-cover border-[0.35rem] shadow-xl rounded-full translate-y-[1px]"
+							priority={true}
+							quality={95}
+							fill
+							src={'/Dvizh.png'}
+							alt=""
+						/>
+					</motion.div>
+					<motion.span
+						initial={{ scale: 0, opacity: 0 }}
+						animate={{ scale: 1, opacity: 1 }}
+						transition={{
+							type: 'spring',
+							stiffness: 50,
+							duration: 0.7,
+							delay: 0.1
+						}}
+						className=" absolute bottom-0 right-0 drop-shadow-lg text-4xl"
+					>
+						😎
+					</motion.span>
+				</div>
+				<motion.p
+					initial={{ scale: 0, opacity: 0, y: 100 }}
+					animate={{ scale: 1, opacity: 1, y: 0 }}
+					transition={{ duration: 0.2 }}
+					className="mb-10 mt-4 px-4 text-2xl font-medium leading-[1.5]"
+				>
+					<b>Hello, my name is Michael</b>. I'm a <u>Front-end / Full-stack</u>{' '}
+					developer <b>with a year</b> and <b> more than 3 years</b> of HTML+CSS{' '}
+					experience. I enjoy web developing. Creating{' '}
+					<i>web-sites and web-apps</i> inspires me. Now focus is{' '}
+					<u>React (Next.js)</u>{' '}
+				</motion.p>
+				<div className=" flex select-none flex-col font-medium sm:flex-row gap-2">
+					<motion.div
+						initial={{ scale: 0, opacity: 0 }}
+						animate={{ scale: 1, opacity: 1 }}
+						transition={{ type: 'spring', stiffness: 200 }}
+						whileHover={{ scale: 1.1 }}
+					>
+						<Link
+							href={'#contact'}
+							className=" cursor-pointer bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full"
+						>
+							Contact me here <BsArrowRight />
+						</Link>
+					</motion.div>
+					<motion.a
+						href="/CV.pdf"
+						download
+						initial={{ scale: 0, opacity: 0 }}
+						animate={{ scale: 1, opacity: 1 }}
+						whileHover={{ scale: 1.1 }}
+						transition={{ type: 'spring', delay: 0.2, stiffness: 200 }}
+						className="  cursor-pointer bg-white text-black px-7 py-3 flex items-center gap-2 rounded-full"
+					>
+						Download my CV <HiDownload />
+					</motion.a>
+					<motion.a
+						href="https://www.linkedin.com/in/mykhailo-romanyuk-84838b255"
+						target="_blank"
+						whileHover={{ scale: 1.1 }}
+						initial={{ scale: 0, opacity: 0 }}
+						animate={{ scale: 1, opacity: 1 }}
+						transition={{ type: 'spring', delay: 0.4, stiffness: 200 }}
+						className="hover:text-black transition-colors hover:bg-gray-500/30 cursor-pointer bg-gray-500/50 text-white text-xl p-4  flex items-center gap-2 rounded-full"
+					>
+						<BsLinkedin />
+					</motion.a>
+					<motion.a
+						href="https://github.com/mugidar"
+						target="_blank"
+						initial={{ scale: 0, opacity: 0 }}
+						animate={{ scale: 1, opacity: 1 }}
+						whileHover={{ scale: 1.1 }}
+						transition={{ type: 'spring', delay: 0.6, stiffness: 200 }}
+						className="hover:text-black transition-colors hover:bg-gray-500/30 cursor-pointer bg-gray-500/50 text-white text-xl p-4  flex items-center gap-2 rounded-full"
+					>
+						<BsGithub />
+					</motion.a>
+				</div>
+			</div>
+		</section>
+	)
+}
+
+export default Intro
