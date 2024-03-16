6d3ac7940eaf07b375e8062bb249e9088f979523
mugidar <mugidar@gmail.com>
2024-03-16 18:29:47 +0200

    SpeedInsights Vercel

--- src/app/(mainpage)/layout.tsx
+++ src/app/(mainpage)/layout.tsx
@@ -1,36 +1,37 @@
-import Header from '@/components/Header'
-import '../globals.css'
-import { Inter } from 'next/font/google'
-import ActiveSectionContextProvider from '@/context/active-section-context'
-import ThemeToggle from '@/components/ThemeToggle'
-import { Toaster } from 'react-hot-toast'
-
-const inter = Inter({ subsets: ['latin'] })
+import Header from "@/components/Header";
+import "../globals.css";
+import { Inter } from "next/font/google";
+import ActiveSectionContextProvider from "@/context/active-section-context";
+import ThemeToggle from "@/components/ThemeToggle";
+import { Toaster } from "react-hot-toast";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-	title: 'MichaelDev | Portfolio',
-	description: `I'm a web developer from Ukraine.`
-}
+  title: "MichaelDev | Portfolio",
+  description: `I'm a web developer from Ukraine.`,
+};
 
 export default function RootLayout({
-	children
+  children,
 }: {
-	children: React.ReactNode
+  children: React.ReactNode;
 }) {
-	return (
-		<html lang="en">
-			<body
-				className={`${inter.className} overflow-x-hidden bg-gray-100 text-inherit relative pt-28`}
-			>
-				<div className="absolute top-[-6rem] w-[31.25rem] h-[31.25rem] right-[11rem]  -z-10 bg-red-100 rounded-full blur-3xl  sm:w-[68.75rem]"></div>
-				<div className="absolute top-[-1rem]  w-[50rem] h-[31.25rem] left-[-35rem] -z-10 bg-blue-100 rounded-full blur-3xl  right-[30rem] sm:w-[68.75rem]  md:left-[-33rem]  lg:left-[-28rem]  xl:left-[-15rem]  2xl:left-[-5rem]"></div>
-				<Toaster />
-				<ActiveSectionContextProvider>
-					<ThemeToggle />
-					<Header />
-					{children}
-				</ActiveSectionContextProvider>
-			</body>
-		</html>
-	)
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.className} overflow-x-hidden bg-gray-100 text-inherit relative pt-28`}
+      >
+        <SpeedInsights />
+        <div className="absolute top-[-6rem] w-[31.25rem] h-[31.25rem] right-[11rem]  -z-10 bg-red-100 rounded-full blur-3xl  sm:w-[68.75rem]"></div>
+        <div className="absolute top-[-1rem]  w-[50rem] h-[31.25rem] left-[-35rem] -z-10 bg-blue-100 rounded-full blur-3xl  right-[30rem] sm:w-[68.75rem]  md:left-[-33rem]  lg:left-[-28rem]  xl:left-[-15rem]  2xl:left-[-5rem]"></div>
+        <Toaster />
+        <ActiveSectionContextProvider>
+          <ThemeToggle />
+          <Header />
+          {children}
+        </ActiveSectionContextProvider>
+      </body>
+    </html>
+  );
 }
